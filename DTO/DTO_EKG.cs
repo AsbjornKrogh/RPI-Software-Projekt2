36e85a964d3cebb5cd86f919b7060584aa9538f8
Peter Gehlert Theilgaard <58323386+thepanda1711@users.noreply.github.com>
2020-03-17 14:59:21 +0100

    part 2

--- DTO/DTO_EKG.cs
+++ DTO/DTO_EKG.cs
@@ -16,6 +16,7 @@ namespace DTO
             global::System.Console.WriteLine("Peter was here");
             global::System.Console.WriteLine("SKIFT DET NAVN");
             global::System.Console.WriteLine("Nu burde det navn være skiftet");
+            global::System.Console.WriteLine("Nu burde det navn være skiftet part 2");
       }
    }
 }
